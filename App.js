deb11eecf2767f0d7df88ec723e291baf36d580a
bassmltaief <bassem.ltaief92@gmail.com>
2021-02-24 16:23:00 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import UserList from "./compenent/UserList";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
